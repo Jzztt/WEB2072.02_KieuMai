9cadf6be8d199ddc5a25fa1b5ebbcdc0b9803271
Jettsyy <teazyyy2512@gmail.com>
2024-09-16 17:58:53 +0700

    feat: template syntax and reactivity

---
+++ TemplateSyntax/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+// 1.Text Interpolation
+const title = 'Template Syntax'
+// cú pháp khai báo + tên biến = giá trị của biến
+
+// 2.Raw HTML
+const content = '<span>Raw HTML</span>'
+
+// 3.Attribute Bindings and Shorthand
+const dynamicId = 'dynamic-id-1'
+
+
+// 4.Boolean Attributes
+
+const isClick = true
+// v-bind:disabled="isClick"
+
+// v-bind cú pháp  cua vue : thuộc tính của thẻ html = biến ở trong phần script
+// : thuộc tính của thẻ html = biến ở trong phần script
+
+// Bài tập, tạo ra 2 thẻ div bao gồm: tên, tuổi và 1 nút button submit
+// thêm id name vào trong thẻ div chứ tên
+// thêm id age vào trong thẻ div chứ tuổi và đổi màu thành màu đỏ
+// thêm id btn vào trong thẻ button
+// và thêm 1 thuộc tính disable  có giá trị là biến btn-disable = true trong script
+
+
+
+// 5.Dynamically Binding Multiple Attributes
+// Truyền động nhiều thuộc tính dữ liệu
+const box = {
+  id:'content1',
+  class: 'content',
+  style: 'padding:10px'
+}
+
+</script>
+
+<template>
+  <div>
+    <div>{{ title }}</div>
+    <span id="content" v-html="content"></span>
+    <p v-bind:id="dynamicId">Lê văn Hiển</p>
+    <button :disabled="isClick"  >click</button>
+    <p v-bind="box">box</p>
+    <!-- Demo bootstrap -->
+     <p class="text-primary">text-primary</p>
+
+  </div>
+</template>
+
+<style>
+#dynamic-id-1{
+  color: yellow
+}
+</style>
---
+++ reactivity/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { nextTick , ref } from 'vue'
+// bước 1: theo dõi
+// nước 2: cập nhật
+
+const count = ref(1)
+
+const handleCount = async () => {
+  count.value++
+  console.log([document.getElementById('count')]);
+  console.log('step1', document.getElementById('count').innerText);
+  await nextTick()
+  console.log('step2', document.getElementById('count').innerText);
+
+}
+// Bài tập 1, Tao 1 ref lưu trữ giá ban đầu là 100, và sau do tạo 1 nut button click, mỗi lần click tăng thêm 1 đơn vị
+// Bài tập 2, hãy lưu dữ liệu cá nhân( tên, tuổi, mssv, sđt, email) và reactivity sau đó hiển thị ra ngoài giao diện
+
+</script>
+
+<template>
+  <div>
+    <div id="count">{{ count }}</div>
+    <button @click="handleCount">Count</button>
+  </div>
+
+</template>
+
+<style>
+
+</style>
